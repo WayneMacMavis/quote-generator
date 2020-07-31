e78704ae3caefde32ca5415d69d3a7e018b9ccdd
Wayne Mac Mavis <w.macmavis83@gmail.com>
2020-07-31 20:40:49 +0200

    first js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('testing');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+// Get quote from API
+async function getQuote() {
+    const proxyUrl = 'http://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+        authorText.innerText = data.quoteAuthor;
+    } catch(error) {
+        getQuote();
+        
+    }
+}
+
+// On load
+getQuote();
