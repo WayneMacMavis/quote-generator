380b3adcac0f11d323e4a44fd89336025170d734
Wayne Mac Mavis <w.macmavis83@gmail.com>
2020-08-01 12:39:48 +0200

    completed

--- script.js
+++ script.js
@@ -3,25 +3,55 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
-// Get quote from API
-async function getQuote() {
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function hideLoadingSpinner() {
+    if(!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+    }
+}
+
+async function getQuoteApi() {
+    showLoadingSpinner();
     const proxyUrl = 'http://cors-anywhere.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     // If author is blank add unknown
     try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    if (data.quoteAuthor = '') {
-        authorText.innerText = data.quoteAuthor;
+    if(data.quoteAuthor === '') {
+        authorText.innerText = 'Unknown';
     } else {
-        quoteText.innerText = data.quoteText;
+        authorText.innerText = data.quoteAuthor;
     }
+    // reduce font size for long quotes
+    if(data.quoteText.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.quoteText;
+    hideLoadingSpinner();
 } catch(error) {
-        getQuote();
+        getQuoteApi();
         
     }
 }
 
-// On load
-getQuote();
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+newQuoteBtn.addEventListener('click', getQuoteApi);
+twitterBtn.addEventListener('click', tweetQuote);
+
+getQuoteApi();
