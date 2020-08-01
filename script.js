25f67a292721db94a14df305d84054790439211d
Wayne Mac Mavis <w.macmavis83@gmail.com>
2020-08-01 19:39:27 +0200

    Js update

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ async function getQuoteApi() {
     showLoadingSpinner();
     const proxyUrl = 'http://cors-anywhere.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    // If author is blank add unknown
+    // If author is blank, add unknown
     try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
